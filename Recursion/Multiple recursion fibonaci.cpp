6277d212fd47b7d5bb51a9c9b2a7de514f9a2b16
Aakash Sharma <aakashsharma121269@gmail.com>
2026-01-21 22:01:42 +0530

    Add Fibonacci function using multiple recursion

---
+++ Recursion/Multiple recursion fibonaci.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fibo(int n){
+    if(n<=1) return n;
+    int last = fibo(n-1);
+    int slast = fibo(n-2);
+
+    return last + slast;
+}
+
+int main()
+{
+    
+    cout<<fibo(4)<<endl;
+    cout<<fibo(6);
+    return 0;
+}
