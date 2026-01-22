e9df14d2f44d188544e1add14ccedab87864fa67
Aakash Sharma <aakashsharma121269@gmail.com>
2026-01-22 23:27:36 +0530

    Implement Tower of Hanoi algorithm in C

---
+++ Recursion/Towerofhanoi.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void TowerOfHanoi(int n,char from,char to,char aux){
+	if(n==1){
+		printf("Move disk 1 from rod %c to rod %c\n ",from,to);
+		return ;
+	}
+	TowerOfHanoi(n-1,from ,aux,to);
+	printf("N1= %d\n",n);
+	printf("Move disk %d from rod %c to rod %c \n ",n,from ,to);
+	printf("N2= %d\n",n);
+	TowerOfHanoi(n-1,aux,to,from);
+}
+int main()
+{
+	int n=3; //no of disks
+	TowerOfHanoi(n,'A','B','C');
+	return 0;
+}
